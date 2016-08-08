cd5232dc2abdc572d4ab3f5f8f23777db2b878a7
Henry Dang <henrydangprg@gmail.com>
2016-08-07 20:41:57 -0400

    Add Circle into Ghost constructor for representation on graph

--- src/com/henrydangprg/triangularmovement/component/Ghost.java
+++ src/com/henrydangprg/triangularmovement/component/Ghost.java
@@ -7,11 +7,13 @@ public class Ghost {
 	private Motor motorLeft;
 	private Motor motorTop;
 	private Motor motorRight;
+	private Circle ghostRepresentation;
 	
-	public Ghost(Motor motorLeft, Motor motorTop, Motor motorRight){
+	public Ghost(Circle ghostRepresentation, Motor motorLeft, Motor motorTop, Motor motorRight){
 		this.motorLeft = motorLeft;
 		this.motorTop = motorTop;
 		this.motorRight = motorRight;
+		this.ghostRepresentation = ghostRepresentation;
 	}
 	
 	public Coordinate getPosition(){
