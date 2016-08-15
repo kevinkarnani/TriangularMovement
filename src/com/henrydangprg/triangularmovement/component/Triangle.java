eec803911112e2f89a11fa88987480de05057d5f
Interverse <jionghengli@gmail.com>
2016-08-15 00:36:40 -0400

    Changed getters/setters to return double[]

--- src/com/henrydangprg/triangularmovement/component/Triangle.java
+++ src/com/henrydangprg/triangularmovement/component/Triangle.java
@@ -5,68 +5,43 @@ import javafx.scene.shape.Polygon;
 
 public class Triangle {
 
-	private final double LEFT_VERTEX_X = 0.0;
-	private final double LEFT_VERTEX_Y = 400.0;
-	private final double TOP_VERTEX_X = 250.0;
-	private final double TOP_VERTEX_Y = 0.0;
-	private final double RIGHT_VERTEX_X = 500.0;
-	private final double RIGHT_VERTEX_Y = 400.0;
-	private Polygon triangle = new Polygon();;
-
-	public Triangle() {
+	public static final double LEFT_VERTEX_X = 0.0;
+	public static final double LEFT_VERTEX_Y = 400.0;
+	public static final double TOP_VERTEX_X = 250.0;
+	public static final double TOP_VERTEX_Y = 0.0;
+	public static final double RIGHT_VERTEX_X = 500.0;
+	public static final double RIGHT_VERTEX_Y = 400.0;
+	private Polygon triangle;
+	
+	public Triangle(){
+		triangle = new Polygon();
 		triangle.getPoints().addAll(
 				new Double[] { LEFT_VERTEX_X, LEFT_VERTEX_Y, TOP_VERTEX_X,
 						TOP_VERTEX_Y, RIGHT_VERTEX_X, RIGHT_VERTEX_Y });
 		triangle.setFill(Color.WHITE);
 	}
-
-	public void sendToBack() {
+	
+	public double[] getTopVertex() {
+		double coord[] = { TOP_VERTEX_X, TOP_VERTEX_Y };
+		return coord;
+	}
+	
+	public double[] getLeftVertex() {
+		double coord[] = { LEFT_VERTEX_X, LEFT_VERTEX_Y };
+		return coord;
+	}
+	
+	public double[] getRightVertex() {
+		double coord[] = { RIGHT_VERTEX_X, RIGHT_VERTEX_Y };
+		return coord;
+	}
+	
+	public void sendToBack(){
 		triangle.toBack();
 	}
 
 	public Polygon getTriangle() {
 		return triangle;
 	}
-
-	/**
-	 * @return the lEFT_VERTEX_X
-	 */
-	public double getLEFT_VERTEX_X() {
-		return LEFT_VERTEX_X;
-	}
-
-	/**
-	 * @return the lEFT_VERTEX_Y
-	 */
-	public double getLEFT_VERTEX_Y() {
-		return LEFT_VERTEX_Y;
-	}
-
-	/**
-	 * @return the tOP_VERTEX_X
-	 */
-	public double getTOP_VERTEX_X() {
-		return TOP_VERTEX_X;
-	}
-
-	/**
-	 * @return the tOP_VERTEX_Y
-	 */
-	public double getTOP_VERTEX_Y() {
-		return TOP_VERTEX_Y;
-	}
-
-	/**
-	 * @return the rIGHT_VERTEX_X
-	 */
-	public double getRIGHT_VERTEX_X() {
-		return RIGHT_VERTEX_X;
-	}
-
-	/**
-	 * @return the rIGHT_VERTEX_Y
-	 */
-	public double getRIGHT_VERTEX_Y() {
-		return RIGHT_VERTEX_Y;
-	}
 }
+
