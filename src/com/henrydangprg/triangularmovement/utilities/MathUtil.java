9e9d1b13ad286f9493dd2052ec65253888671d60
Interverse <jionghengli@gmail.com>
2016-08-16 23:07:55 -0400

    Add Math Functions

---
+++ src/com/henrydangprg/triangularmovement/utilities/MathUtil.java
@@ -0,0 +1,37 @@
+package com.henrydangprg.triangularmovement.utilities;
+
+import com.henrydangprg.triangularmovement.component.Coordinate;
+
+public class MathUtil {
+	public static double calcSpeed(double start, double end, double time) {
+        return (end - start) / time;
+    }
+
+    public static double distanceBetweenPoints(Coordinate coord1, Coordinate coord2) {
+        return Math.sqrt(Math.pow(coord2.getX() - coord1.getX(), 2) + Math.pow(coord2.getY() - coord1.getY(), 2));
+    }
+
+    public static double findSlope(Coordinate coord1, Coordinate coord2) {
+        return (coord2.getY() - coord1.getY()) / (coord2.getX() - coord1.getX());
+    }
+
+    public static double findYIntercept(double slope, Coordinate coord) {
+        return -slope * coord.getX() + coord.getY();
+    }
+
+    public static double[] findVectorComponents(double magnitude, double angle) {
+        double[] vectorComponents = { magnitude * Math.cos(angle),
+                                      magnitude * Math.sin(angle) };
+        return vectorComponents;
+    }
+
+    public static boolean checkInequality(double slope, double yintercept, Coordinate coord, boolean isMore) {
+        if (isMore) {
+            if (coord.getY() >= slope * coord.getX() + yintercept) return true;
+        } else {
+            if (coord.getY() <= slope * coord.getX() + yintercept) return true;
+        }
+
+        return false;
+    }
+}
